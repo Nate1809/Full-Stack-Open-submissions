08efd1c19e84562af83a42c1f0042e80fa0d2aba
Nathan Guzman <monone18@me.com>
2025-05-17 04:49:59 -0500

    Solve Part 1 - 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,37 +1,30 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course}/>
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Footer 
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Footer parts={parts} />
     </div>
   )
 }
 
 const Header = (props) => {
-  console.log(props)
   return (
     <>
       <h1>{props.course}</h1>
@@ -40,27 +33,30 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
+  const part1 = props.parts[0]
+  const part2 = props.parts[1]
+  const part3 = props.parts[2]
+
   return (
     <>
       <Part
-        part={props.part1.name}
-        exercises={props.part1.exercises}
+        part={part1.name}
+        exercises={part1.exercises}
       />
       <Part
-        part={props.part2.name}
-        exercises={props.part2.exercises}
+        part={part2.name}
+        exercises={part2.exercises}
       />
       <Part
-        part={props.part3.name}
-        exercises={props.part3.exercises}
+        part={part3.name}
+        exercises={part3.exercises}
       />
     </>
   )
 }
 
 const Part = (props) => {
-  console.log(props)
+
   return (
     <>
       <p>
@@ -71,10 +67,13 @@ const Part = (props) => {
 }
 
 const Footer = (props) => {
-  console.log(props)
+  const part1 = props.parts[0]
+  const part2 = props.parts[1]
+  const part3 = props.parts[2]
+
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </>
   )
 }
