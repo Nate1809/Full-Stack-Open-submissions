fdf6cabd32e0a9ccad8c41a8f3dbf8e96c9eaa04
Nathan Guzman <monone18@me.com>
2025-05-29 17:29:36 -0500

    2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 import CountryBasic from "./CountryBasic"; 
 import CountryFull from "./CountryFull";
 
-const Countries = ( {countries, newFilter, handleShow} ) => {
+const Countries = ( {countries, newFilter, onFilterReset} ) => {
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    const handleShow = (country) => setSelectedCountry(country);
+    const handleBack = () => {
+        setSelectedCountry(null);
+        // Reset filter when going back from a filtered single country view
+        if (newFilter !== "") {
+            onFilterReset();
+        }
+    };
+
     const countriesToShow = newFilter === ""
         ? countries
         // if condition is true we add to list
@@ -9,13 +22,15 @@ const Countries = ( {countries, newFilter, handleShow} ) => {
             country.name.common.toLowerCase().includes(newFilter.toLowerCase())
         )
 
-    // Handle different display options
+    // Display single country on show button click
+    if (selectedCountry) {
+        return <CountryFull country={selectedCountry} handleBack={handleBack}/>;
+    }
 
+    // Display different number of countries
     if (countriesToShow.length === 1) {
-        console.log(countriesToShow[0])
-        return (
-            <CountryFull country={countriesToShow[0]}/>
-        )
+        setSelectedCountry(countriesToShow[0])
+        return <CountryFull country={countriesToShow[0]} handleBack={handleBack}/>
     } else if (countriesToShow.length <= 10) {
         return (
             <ul>
