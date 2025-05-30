c4a09072ffebb1e20e1685a0b3127388334a2e61
Nathan Guzman <monone18@me.com>
2025-05-29 19:15:17 -0500

    Add weather text

--- part2/countries/src/components/CountryFull.jsx
+++ part2/countries/src/components/CountryFull.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from 'react'
+
+import weatherService from '../services/weather'
+
 const CountryFull = ( {country, handleBack} ) => {
+
+    const [weather, setWeather] = useState(null)
+
+    // Get countries from api
+    useEffect( () => {
+        weatherService
+            .getWeather(country.name.common)
+            .then(curWeather => {
+                console.log('promise fulfilled, weather fetched from server')
+                setWeather(curWeather)
+            })
+    }, [])
+
     return (
         <div>
             <h1>{country.name.common}</h1>
@@ -13,6 +30,15 @@ const CountryFull = ( {country, handleBack} ) => {
             </ul>
             {/* Flag */}
             <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
+            
+            {weather ? (
+                <div>
+                    <p>Temperature: {weather.main.temp}°C</p>
+                    <p>Wind Speed: {weather.wind.speed} m/s</p>
+                </div>
+            ) : (
+                <p>Loading weather...</p>
+            )}
             <div><button onClick={handleBack}>Back</button></div>
         </div>
     )
