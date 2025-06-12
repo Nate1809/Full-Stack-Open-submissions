af2dae917c6b12c4e435eda32044148a30c92d61
Nathan Guzman <monone18@me.com>
2025-06-12 16:27:26 -0500

    Part 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -6,141 +6,147 @@ import Notification from "./components/Notification";
 import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [newFilter, setNewFilter] = useState("");
-  const [notification, setNotification] = useState({ message: null, isError: false })
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [newFilter, setNewFilter] = useState("");
+    const [notification, setNotification] = useState({message: null, isError: false})
 
-  // Get persons from json server
-  useEffect( () => {
-    personService
-      .getAll()
-      .then(initialPersons => {
-        console.log('promise fulfilled, data fetched from server')
-        setPersons(initialPersons)
-      })
-  }, [])
-
-  // Helper function for notifications
-  const showNotification = (message, isError = false) => {
-    setNotification({ message, isError })
-    setTimeout(() => {
-      setNotification({ message: null, isError: false })
-    }, 5000)
-  }
-
-  // Form submit handler
-  const addContact = (event) => {
-    event.preventDefault();
-    const contactObject = {
-      name: newName,
-      number: newNumber
-    };
-    // Check if name already exists and handle it
-    const personNames = persons.map((person) => person.name);
-    if (personNames.includes(newName)) {
-      console.log(`${newName} already in Phonebook`);
-      if( window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`) ){
-        // update number in backend
-        const existing_person = persons.find(n => n.name === newName)
-        const changedPerson = { ...existing_person, number: newNumber }
+    // Get persons from json server
+    useEffect(() => {
         personService
-          .update(existing_person.id, changedPerson)
-          .then(returnedPerson => {
-            setPersons(persons.map(person => person.id === existing_person.id ? returnedPerson : person))
-            // show notification
-            showNotification(`Updated ${returnedPerson.name}'s number`, false)
-            // reset states
-            setNewName('')
-            setNewNumber('')
-          })
-          .catch(error => {
-            console.error('Failed to update person:', error)
-            showNotification(`Error: Failed to update contact (${person.name})`, true) // error
-          })
-      }
-      return;
+            .getAll()
+            .then(initialPersons => {
+                console.log('promise fulfilled, data fetched from server')
+                setPersons(initialPersons)
+            })
+    }, [])
+
+    // Helper function for notifications
+    const showNotification = (message, isError = false) => {
+        setNotification({message, isError})
+        setTimeout(() => {
+            setNotification({message: null, isError: false})
+        }, 5000)
     }
 
-    // ELSE, Add contact to backend server
-    personService
-      .create(contactObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        // show success notification
-        showNotification(`Added ${returnedPerson.name}`, false)
-        // Reset states
-        setNewName('')
-        setNewNumber('')
-        // print request body
-        console.log(`Added ${returnedPerson.name} with number ${returnedPerson.number} and id ${returnedPerson.id}`)
-      })
-  };
+    // Form submit handler
+    const addContact = (event) => {
+        event.preventDefault();
+        const contactObject = {
+            name: newName,
+            number: newNumber
+        };
+        // Check if name already exists and handle it
+        const personNames = persons.map((person) => person.name);
+        if (personNames.includes(newName)) {
+            console.log(`${newName} already in Phonebook`);
+            if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+                // update number in backend
+                const existing_person = persons.find(n => n.name === newName)
+                const changedPerson = {...existing_person, number: newNumber}
+                personService
+                    .update(existing_person.id, changedPerson)
+                    .then(returnedPerson => {
+                        setPersons(persons.map(person => person.id === existing_person.id ? returnedPerson : person))
+                        // show notification
+                        showNotification(`Updated ${returnedPerson.name}'s number`, false)
+                        // reset states
+                        setNewName('')
+                        setNewNumber('')
+                    })
+                    .catch(error => {
+                        console.log(error.response.data.error)
+                        console.error('Failed to update person:', error)
+                        showNotification(`Error: Failed to update contact (${person.name})`, true) // error
+                    })
+            }
+            return;
+        }
 
-  const handleNameChange = (event) => {
-    // console.log(event.target.value)
-    setNewName(event.target.value);
-  };
+        // ELSE, Add contact to backend server
+        personService
+            .create(contactObject)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson))
+                // show success notification
+                showNotification(`Added ${returnedPerson.name}`, false)
+                // Reset states
+                setNewName('')
+                setNewNumber('')
+                // print request body
+                console.log(`Added ${returnedPerson.name} with number ${returnedPerson.number} and id ${returnedPerson.id}`)
+            })
+            .catch(error =>{
+                console.error('Failed to update person:', error)
+                showNotification(`Error: Failed to update contact. ${error.response.data.error}`, true) // error
 
-  const handleNumberChange = (event) => {
-    // console.log(event.target.value)
-    setNewNumber(event.target.value);
-  };
+            })
+    };
 
-  const handleFilterChange = (event) => {
-    // console.log(event.target.value)
-    setNewFilter(event.target.value);
-  }
+    const handleNameChange = (event) => {
+        // console.log(event.target.value)
+        setNewName(event.target.value);
+    };
 
-  const handleDelete = id => {
-    const person = persons.find(p => p.id === id)
-    if(window.confirm(`Delete ${person.name}?`)){
-      personService
-        .deletePerson(id)
-        .then(() => {
-          setPersons(persons.filter(person => person.id !== id))
-          showNotification(`Deleted ${person.name}`, false)
-        })
-        .catch( error => {
-          setPersons(persons.filter(person => person.id !== id))
-          console.error('Failed to delete person:', error)
-          showNotification(`Information of ${person.name} has already been removed from server`, true)
-        })
+    const handleNumberChange = (event) => {
+        // console.log(event.target.value)
+        setNewNumber(event.target.value);
+    };
+
+    const handleFilterChange = (event) => {
+        // console.log(event.target.value)
+        setNewFilter(event.target.value);
     }
-  }
 
-  return (
-    <div>
-      {/* Header */}
-      <h2>Phonebook</h2>
-      {/* Show notification */}
-      <Notification message={notification.message} isError={notification.isError}/>
+    const handleDelete = id => {
+        const person = persons.find(p => p.id === id)
+        if (window.confirm(`Delete ${person.name}?`)) {
+            personService
+                .deletePerson(id)
+                .then(() => {
+                    setPersons(persons.filter(person => person.id !== id))
+                    showNotification(`Deleted ${person.name}`, false)
+                })
+                .catch(error => {
+                    setPersons(persons.filter(person => person.id !== id))
+                    console.error('Failed to delete person:', error)
+                    showNotification(`Information of ${person.name} has already been removed from server`, true)
+                })
+        }
+    }
 
-      {/* filter contacts by name */}
-      <Filter value={newFilter} onChange={handleFilterChange}/>
+    return (
+        <div>
+            {/* Header */}
+            <h2>Phonebook</h2>
+            {/* Show notification */}
+            <Notification message={notification.message} isError={notification.isError}/>
 
-      {/* Form */}
-      <h2>add a new</h2>
-      <PersonForm 
-        onSubmit={addContact}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
+            {/* filter contacts by name */}
+            <Filter value={newFilter} onChange={handleFilterChange}/>
 
-      {/* Phonebook */}
-      <h2>Numbers</h2>
+            {/* Form */}
+            <h2>add a new</h2>
+            <PersonForm
+                onSubmit={addContact}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newNumber={newNumber}
+                handleNumberChange={handleNumberChange}
+            />
 
-      <Persons persons={persons} newFilter={newFilter} handleDelete={handleDelete}/>
+            {/* Phonebook */}
+            <h2>Numbers</h2>
 
-      {/*  DEBUG SHOWW STATES */}
-      {/* <div>debug name state: {newName}</div>
+            <Persons persons={persons} newFilter={newFilter} handleDelete={handleDelete}/>
+
+            {/*  DEBUG SHOWW STATES */}
+            {/* <div>debug name state: {newName}</div>
       <div>debug number state: {newNumber}</div> */}
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default App;
--- part3/phone-backend/index.js
+++ part3/phone-backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 
@@ -11,127 +13,134 @@ app.use(express.static('dist')) // this makes it so that we can access the files
 
 // Logging middleware using morgan
 morgan.token('body', (req) => {
-    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-// }
-// app.use(requestLogger)
-
-// hardcoded data
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 // API ROUTES
 
 // GET all persons
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      console.log('Got persons from database:', persons)
+      response.json(persons)
+    })
 })
 
 // GET a single person
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      console.log('Got person from database:', person)
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
 
 // GET info about the phonebook
 app.get('/info', (request, response) => {
-    const numEntries = persons.length
-    response.send(
-        `<p>Phonebook has info for ${numEntries} people</p> 
-                <p>${new Date()}</p>`
+  Person.collection
+    .countDocuments()
+    .then(numEntries => {
+      console.log(`Phonebook has info for ${numEntries} people`)
+      response.send(
+        `<p>Phonebook has info for ${numEntries} people</p>
+                        <p>${new Date()}</p>`
+      )
+    }
     )
 })
 
 // DELETE a person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    console.log("Deleted person with id: " + id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then( () => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateID = () => {
-    let newId
-    do {
-        newId = Math.floor(Math.random() * 1_000_000).toString()
-    } while (persons.find(person => person.id === newId))
-    return newId
-}
-
 //POST a new person
-app.post('/api/persons', (request, response) => {
-    console.log('POST request received')
-    const body = request.body
+app.post('/api/persons', (request, response, next) => {
+  console.log('POST request received')
+  const body = request.body
 
-    // Check if the name and number are in the request body
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name and number are required'
-        })
-    }
+  // Check if the name and number are in the request body
+  // if (!body.name || !body.number) {
+  //     return response.status(400).json({
+  //         error: 'name and number are required'
+  //     })
+  // }
 
-    // Check if the name already exists
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+  // // Check if the name already exists
+  // if (persons.find(person => person.name === body.name)) {
+  //     return response.status(400).json({
+  //         error: 'name must be unique'
+  //     })
+  // }
 
-    const person = {
-        id: generateID(),
-        name: body.name,
-        number: body.number
-    }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    persons.push(person) // ✅ actually modifies the array
-    response.json(person)
+  person
+    .save()
+    .then(savedPerson => {
+      console.log('Person saved to database')
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+//Update single person
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(400).end()
+      }
+
+      person.name = name
+      person.number = number
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 // Error handling middleware
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
+// error handling middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 // Finally, we need to start our server
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
