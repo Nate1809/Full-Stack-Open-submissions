5b757f5dba9d7a153c47539f4e776c17c17c4cc0
Nathan Guzman <monone18@me.com>
2025-06-16 14:00:16 -0500

    4.8

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Bolillos del mundo",
+    author: "Alameda Smith",
+    url: "https://bullterrier.world/blogs/all/bull-terriers-sunbathing-the-health-benefits-of-vitamin-d",
+    likes: 5999
+  },
+  {
+    title: "Stardew Valley Cookbook is now avaialble",
+    author: "Eric Barone",
+    url: "https://www.stardewvalley.net/the-stardew-valley-cookbook-is-now-available/",
+    likes: 5999
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
