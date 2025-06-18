83399d1a59ebd50a532cb227f7bef2e1b5c67c54
Nathan Guzman <monone18@me.com>
2025-06-18 15:06:27 -0500

    5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import NewBlogForm from "./components/NewBlogForm.jsx";
+import NewBlogForm from "./components/NewBlogForm.jsx"
+import Notification from "./components/Notification.jsx"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-
+  const [notification, setNotification] = useState({message: null, isError: false})
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
@@ -46,12 +47,19 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      showNotification('Logged in')
     } catch (exception) {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      // Show notification
+      showNotification('Wrong username or password', true)
     }
+  }
+
+  // Helper function for notifications
+  const showNotification = (message, isError = false) => {
+    setNotification({message, isError})
+    setTimeout(() => {
+      setNotification({message: null, isError: false})
+    }, 5000)
   }
 
   const handleCreate = async (event) => {
@@ -59,11 +67,13 @@ const App = () => {
     try {
       const newBlog = await blogService.create({ title, author, url })
       setBlogs(blogs.concat(newBlog))
+      showNotification(`A new blog ${title} by ${author} added`)
       setTitle('')
       setAuthor('')
       setUrl('')
     } catch (error) {
       console.error(error)
+      showNotification('Blog creation failed', true)
     }
   }
 
@@ -99,13 +109,22 @@ const App = () => {
     setUser(null)
     window.localStorage.removeItem('loggedBloglistUser')
     blogService.setToken(null)
+
   }
 
   return (
     <div>
+      {/*Header*/}
+      <h2>blogs</h2>
+
+      {/*Show notification*/}
+      <Notification message={notification.message} isError={notification.isError} />
+
+      {/*Login*/}
       {!user && loginForm()}
+
+      {/*Show blogs*/}
       {user && <div>
-        <h2>blogs</h2>
         <p>{user.name} logged in <button onClick={ handleLogout }>logout</button></p>
         <NewBlogForm
           title={title}
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,35 @@
+const Notification = ({ message, isError }) => {
+    if (message === null) {
+        return null
+    }
+
+    const errorStyle = {
+        color: 'red',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px'
+    }
+
+    const successStyle = {
+        color: 'green',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px'
+    }
+
+    const notificationStyle = isError ? errorStyle : successStyle
+
+    return (
+        <div style={notificationStyle}>
+            {message}
+        </div>
+    )
+}
+
+export default Notification
