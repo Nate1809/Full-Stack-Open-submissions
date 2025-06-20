d8a68494c5c3ae1fc043e24648980f43d71a63a5
Nathan Guzman <monone18@me.com>
2025-06-20 14:27:46 -0500

    5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -17,8 +17,10 @@ const App = () => {
   const blogFormRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+    blogService.getAll()
+      .then(blogs => blogs.sort((a, b) => b.likes - a.likes))
+      .then(sortedBlogs =>
+      setBlogs( sortedBlogs )
     )
   }, [])
 
