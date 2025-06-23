49f453d032e4e21f63614328308022d4a7ad7bfa
Nathan Guzman <monone18@me.com>
2025-06-23 14:10:34 -0500

    5.16

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import NewBlogForm from "./NewBlogForm.jsx";
 
 describe('Blog', () => {
   const blog = {
@@ -37,10 +38,57 @@ describe('Blog', () => {
 
   test('URL and likes displayed when show button clicked', async () => {
     const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
     const div = container.querySelector('.detailBlogContent')
     expect(div).not.toHaveStyle('display: none')
   })
+
+  test('if the like button is clicked twice, the event handler the component received as props is called twice', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
+  })
+
+})
+
+describe('New Blog Form', () => {
+
+  const mockCreateHandler = vi.fn()
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <NewBlogForm createBlog={ mockCreateHandler }/>
+    ).container
+  })
+
+  test('new blog form calls the event handler it received as props with the right details', async () => {
+    // screen.debug()
+    const user = userEvent.setup()
+
+    const titleInput = screen.getByPlaceholderText('title')
+    const authorInput = screen.getByPlaceholderText('author')
+    const urlInput = screen.getByPlaceholderText('url')
+    const createButton = screen.getByText('create')
+
+    await user.type(titleInput, 'bolillitos del mundo')
+    await user.type(authorInput, 'Raymix')
+    await user.type(urlInput, 'https://www.akc.org/dog-breeds/bull-terrier/')
+    await user.click(createButton)
+
+    // console.log(mockCreateHandler.mock.calls[0][0])
+    expect(mockCreateHandler.mock.calls).toHaveLength(1)
+    expect(mockCreateHandler.mock.calls[0][0].title).toBe('bolillitos del mundo')
+    expect(mockCreateHandler.mock.calls[0][0].author).toBe('Raymix')
+    expect(mockCreateHandler.mock.calls[0][0].url).toBe('https://www.akc.org/dog-breeds/bull-terrier/')
+  })
+
 })
