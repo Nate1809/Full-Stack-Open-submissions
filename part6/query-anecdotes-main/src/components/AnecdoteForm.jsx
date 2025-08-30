3a4486ac5b049dcfe4d2b9e3d597fc4618722d8d
Nathan Guzman <monone18@me.com>
2025-08-30 14:37:10 -0500

    6.24

--- part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -1,14 +1,22 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {createAnecdote} from "../requests.js";
+import { useNotificationDispatch } from "../notificationContext.jsx";
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const notificationDispatch = useNotificationDispatch()
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    },
+    onError: (error) => {
+      notificationDispatch({ type: "SET", value: "too short anecdote, must have length 5 or more" })
+      setTimeout(() => {
+        notificationDispatch({ type: "CLEAR" })
+      }, 5000)
     }
   })
 
@@ -16,15 +24,7 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    
-    if (content.length < 5) {
-      console.log('anecdote too short, must have length 5 or more')
-      return
-    }
-    
     newAnecdoteMutation.mutate({ content, votes: 0 })
-    console.log('new anecdote')
-    console.log(content)
 }
 
   return (
