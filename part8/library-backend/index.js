919549df15f4d0186359563bc69833002febbac4
Nathan Guzman <monone18@me.com>
2025-11-11 14:24:03 -0600

    8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+// const { v1: uuid } = require('uuid')
+// const { GraphQLError } = require('graphql')
 
 let authors = [
   {
@@ -28,17 +30,9 @@ let authors = [
 ]
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
  * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
 */
 
 let books = [
@@ -98,9 +92,24 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String]!
+  }
+  
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+  
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
   }
 `
 
@@ -108,6 +117,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
