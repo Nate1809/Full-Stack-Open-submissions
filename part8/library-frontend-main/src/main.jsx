001ddb4580d6861fd73ee2e9be2ed6c64c9b0f3b
Nathan Guzman <monone18@me.com>
2025-11-14 11:28:04 -0600

    8.8

---
+++ part8/library-frontend-main/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, gql } from '@apollo/client';
+
+// explicitly define the HttpLink
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
